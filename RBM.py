224a42dd6c78b1bdb206f6afc40ac8b73a02ed90
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2019-01-06 23:37:07 +0900

    Add files via upload

--- RBM.py
+++ RBM.py
@@ -10,20 +10,43 @@ def getData():
 
     data = [] # training data
     cols = 0 # number of columns (attribute and answer)
-    for i in range(len(read)):
+
+    # read number of hidden units
+    hiddens = int(read[0])
+
+    # read data
+    for i in range(1, len(read)-1):
         read[i] = read[i].replace('\n', '')
         row = read[i].split(' ')
         for i in range(len(row)):
             row[i] = int(row[i])
         data.append(row)
 
-    return data
+    # read input data
+    inputD = read[len(read)-1].split(' ')
+
+    return (hiddens, data, inputD)
 
 def sigmoid(value):
-    return round(1 / (1 + math.exp(-value)), 2)
+    return round(1 / (1 + math.exp(-value)), 6)
+
+# make random value
+def makeRandom(value):
+    result = 0
+    for i in range(value):
+        if random.random() >= 0.5: result += 1/value
+    result = round(result, 6)
+    return result
 
 # main: RBM
-def RBM(v, W):
+def RBM(v, colsOfW, lr):
+    
+    # initialize weight
+    W = [[0]*colsOfW for i in range(len(v[0]))]
+    for i in range(len(W)):
+        for j in range(len(W[0])):
+            W[i][j] = round((-1) + makeRandom(20)*2, 6)
+    
     # print
     print('v:')
     for i in range(len(v)):
@@ -33,71 +56,120 @@ def RBM(v, W):
     for i in range(len(W)):
         print(W[i])
     print('')
+
+    # repeat until convergence
+    count = 0
+    while(1):
+        count += 1
+        print('ROUND ' + str(count))
     
-    # 1. p(h|v) = sigm(vW)
-    phv = [[0]*len(W[0]) for i in range(len(v))]
-    for i in range(len(v)):
-        for j in range(len(W[0])):
-           for k in range(len(W)):
-               phv[i][j] += v[i][k] * W[k][j]
-           phv[i][j] = sigmoid(phv[i][j])
+        # 1. p(h|v) = sigm(vW)
+        phv = [[0]*len(W[0]) for i in range(len(v))]
+        for i in range(len(v)):
+            for j in range(len(W[0])):
+               for k in range(len(W)):
+                   phv[i][j] += v[i][k] * W[k][j]
+               phv[i][j] = sigmoid(phv[i][j])
 
-    print('p(h|v) = sigm(vW)')
-    for i in range(len(phv)):
-        print(phv[i])
-    print('')
+        print('p(h|v) = sigm(vW)')
+        for i in range(len(phv)):
+            print(phv[i])
+        print('')
 
-    # 2. h = {p(h|v) >= random value}
-    h = [[0]*len(W[0]) for i in range(len(v))]
-    for i in range(len(v)):
-        for j in range(len(W[0])):
-            if phv[i][j] >= 0.5: h[i][j] = 1
-            else: h[i][j] = 0
+        # 2. h = {p(h|v) >= random value}
+        h = [[0]*len(W[0]) for i in range(len(v))]
+        for i in range(len(v)):
+            for j in range(len(W[0])):
+                if phv[i][j] >= makeRandom(10): h[i][j] = 1
+                else: h[i][j] = 0
 
-    print('h = {p(h|v) >= randomval}')
-    for i in range(len(h)):
-        print(h[i])
-    print('')
+        print('h = {p(h|v) >= randomval}')
+        for i in range(len(h)):
+            print(h[i])
+        print('')
 
-    # 3. p(v1|h) = sigm(h*W^T)
-    pv1h = [[0]*len(v[0]) for i in range(len(v))]
-    for i in range(len(v)):
-        for j in range(len(W)):
-            for k in range(len(W[0])):
-                pv1h[i][j] += h[i][k] * W[j][k]
-            pv1h[i][j] = sigmoid(pv1h[i][j])
+        # 3. p(v1|h) = sigm(h*W^T)
+        pv1h = [[0]*len(v[0]) for i in range(len(v))]
+        for i in range(len(v)):
+            for j in range(len(W)):
+                for k in range(len(W[0])):
+                    pv1h[i][j] += h[i][k] * W[j][k]
+                pv1h[i][j] = sigmoid(pv1h[i][j])
 
-    print('p(v1|h) = sigm(h*W^T)')
-    for i in range(len(pv1h)):
-        print(pv1h[i])
-    print('')
+        print('p(v1|h) = sigm(h*W^T)')
+        for i in range(len(pv1h)):
+            print(pv1h[i])
+        print('')
 
-    # 4. v(1) = {p(v1|h) >= random value}
-    v1 = [[0]*len(v[0]) for i in range(len(v))]
-    for i in range(len(v)):
-        for j in range(len(W)):
-            if pv1h[i][j] >= 0.5: v1[i][j] = 1
-            else: v1[i][j] = 0
+        # 4. v(1) = {p(v1|h) >= random value}
+        v1 = [[0]*len(v[0]) for i in range(len(v))]
+        for i in range(len(v)):
+            for j in range(len(W)):
+                if pv1h[i][j] >= makeRandom(10): v1[i][j] = 1
+                else: v1[i][j] = 0
 
-    print('v1 = {p(v1|h) >= randomval}')
-    for i in range(len(v1)):
-        print(v1[i])
-    print('')
+        print('v1 = {p(v1|h) >= randomval}')
+        for i in range(len(v1)):
+            print(v1[i])
+        print('')
 
-    # 5. h(1) = {sigm(v(1)*W) >= randomval}
-    h1 = [[0]*len(W[0]) for i in range(len(v1))]
-    for i in range(len(v1)):
-        for j in range(len(W[0])):
-           for k in range(len(W)):
-               h1[i][j] += v1[i][k] * W[k][j]
-           if sigmoid(h1[i][j]) >= 0.5: h1[i][j] = 1
-           else: h1[i][j] = 0
+        # 5. h(1) = {sigm(v1*W) >= randomval}
+        h1 = [[0]*len(W[0]) for i in range(len(v1))]
+        for i in range(len(v1)):
+            for j in range(len(W[0])):
+               for k in range(len(W)):
+                   h1[i][j] += v1[i][k] * W[k][j]
+               if sigmoid(h1[i][j]) >= makeRandom(10): h1[i][j] = 1
+               else: h1[i][j] = 0
 
-    print('h(1) = {sigm(v(1)*W) >= randomval}')
-    for i in range(len(h1)):
-        print(h1[i])
-    print('')
+        print('h(1) = {sigm(v(1)*W) >= randomval}')
+        for i in range(len(h1)):
+            print(h1[i])
+        print('')
 
-weight = [[0.2, -0.2], [-0.7, 0.1], [0.8, -0.5], [-0.4, -0.2], [0.2, 0.4], [-0.1, 0.1]]
-data = getData()
-RBM(data, weight)
+        # 6. dW = lr*(v^T*h - v1^T*h1)
+        vTh = [[0]*len(W[0]) for i in range(len(W))] # array for v^T*h
+        v1Th1 = [[0]*len(W[0]) for i in range(len(W))] # array for v1^T*h1
+        dW = [[0]*len(W[0]) for i in range(len(W))] # array for dW
+        
+        for i in range(len(W)):
+            for j in range(len(W[0])):
+                vTh[i][j] = 0
+                v1Th1[i][j] = 0
+                for k in range(len(v1)):
+                    vTh[i][j] += v[k][i] * h[k][j]
+                    v1Th1[i][j] += v1[k][i] * h1[k][j]
+                    dW[i][j] = lr*(vTh[i][j] - v1Th1[i][j]) # calculate dW
+
+                    # truncate error
+                    vTh[i][j] = round(vTh[i][j], 6)
+                    v1Th1[i][j] = round(v1Th1[i][j], 6)
+                    dW[i][j] = round(dW[i][j], 6)
+
+        print('dW = (learning rate)*(v^T*h - v(1)^T*h(1))')
+        for i in range(len(dW)):
+            print(dW[i])
+        print('')
+        
+        # 7. W^(1) = W^(0)+dW
+        updateSum = 0
+        for i in range(len(W)):
+            for j in range(len(W[0])):
+                W[i][j] += dW[i][j]
+                updateSum += abs(dW[i][j])
+                
+                W[i][j] = round(W[i][j], 6) # truncate error
+
+        print('updated W: W(t+1) = W(t)+dW')
+        for i in range(len(W)):
+            print(W[i])
+        print('sum of update: ' + str(updateSum))
+        print('')
+
+        # if converged, break
+        if updateSum < len(W)*len(W[0])*0.001:
+            return W
+            break
+
+(hiddens, data, inputD) = getData()
+weight = RBM(data, hiddens, 0.5)
