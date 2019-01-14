96b5deefd89b92cbfeb0f329f9fba0fde3811a3f
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2019-01-14 22:15:10 +0900

    Add files via upload

--- CNN.py
+++ CNN.py
@@ -14,6 +14,7 @@ def getData():
     images = int(info[2]) # number of images
     filtersize = int(info[3]) # number of rows and columns of each filter
     filters = int(info[4]) # number of filters
+    prt = int(info[5]) # print?
 
     # data -> imgdata -> symbol and (content -> each row -> each pixel)
     data = []
@@ -45,7 +46,7 @@ def getData():
         testimgcontent.append(read[images*(rows+1)+i+2].split('\n')[0])
     testimgdata.append(testimgcontent)
 
-    return (data, rows, cols, images, filtersize, filters, testimgdata)
+    return (data, rows, cols, images, filtersize, filters, testimgdata, prt)
 
 # . or # to 0 or 1
 def toVal(value):
@@ -73,29 +74,30 @@ def convolutionImageFilter(data, rows, cols, img, i, filtersize, flts):
     return temp
 
 # convolution -> return convolution result array
-def convolution(data, rows, cols, images, filtersize, filters, flts):
+def convolution(data, rows, cols, images, filtersize, filters, flts, prt):
     convol = [] # result of convolution 0
     for img in range(images):
         convolOfImg = [] # convolution result of each image
-        print('** Name of image: ' + str(data[img][0]) + ' **')
+        if prt != 0: print('** Name of image: ' + str(data[img][0]) + ' **')
         convolOfImg.append(data[img][0])
 
         # unit: each image
         imgData = []
         for i in range(filters):
-            print('< Filter ' + str(i) + ' >')
+            if prt != 0: print('< Filter ' + str(i) + ' >')
 
             # unit: each filter
             temp = convolutionImageFilter(data, rows, cols, img, i, filtersize, flts)
             imgData.append(temp)
 
             # print result
-            for j in range(len(temp)):
-                printStr = '['
-                for k in range(len(temp[0])):
-                    printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
-                print(str(printStr) + ' ]')
-            print('')
+            if prt != 0:
+                for j in range(len(temp)):
+                    printStr = '['
+                    for k in range(len(temp[0])):
+                        printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
+                    print(str(printStr) + ' ]')
+                print('')
             
         convolOfImg.append(imgData)
         convol.append(convolOfImg)
@@ -103,7 +105,7 @@ def convolution(data, rows, cols, images, filtersize, filters, flts):
     return convol
 
 # ReLU of convolution result -> return ReLU result
-def ReLU(convol):
+def ReLU(convol, prt):
 
     # len(convol) = number of images
     # len(convol[0][1]) = number of filters
@@ -113,13 +115,13 @@ def ReLU(convol):
     ReLU = [] # result of ReLU
     for img in range(len(convol)):
         ReLUOfImg = [] # ReLU result of each image
-        print('** Name of image: ' + str(convol[img][0]) + ' **')
+        if prt != 0: print('** Name of image: ' + str(convol[img][0]) + ' **')
         ReLUOfImg.append(convol[img][0])
 
         # unit: each image
         imgData = []
         for i in range(len(convol[0][1])):
-            print('< Filter ' + str(i) + ' >')
+            if prt != 0: print('< Filter ' + str(i) + ' >')
 
             # unit: each filter
             temp = []
@@ -134,12 +136,13 @@ def ReLU(convol):
             imgData.append(temp)
 
             # print result
-            for j in range(len(temp)):
-                printStr = '['
-                for k in range(len(temp[0])):
-                    printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
-                print(str(printStr) + ' ]')
-            print('')
+            if prt != 0:
+                for j in range(len(temp)):
+                    printStr = '['
+                    for k in range(len(temp[0])):
+                        printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
+                    print(str(printStr) + ' ]')
+                print('')
         ReLUOfImg.append(imgData)
             
         ReLU.append(ReLUOfImg)
@@ -147,7 +150,7 @@ def ReLU(convol):
     return ReLU
 
 # Pooling of ReLU result -> return Pooling result
-def pooling(ReLU, poolFiltSize, stride, option):
+def pooling(ReLU, poolFiltSize, stride, option, prt):
 
     # len(ReLU) = number of images
     # len(ReLU[0][1]) = number of filters
@@ -160,7 +163,7 @@ def pooling(ReLU, poolFiltSize, stride, option):
     Pooling = [] # result of pooling
     for img in range(len(ReLU)):
         PoolingOfImg = [] # pooling result of each image
-        print('** Name of image: ' + str(ReLU[img][0]) + ' **')
+        if prt != 0: print('** Name of image: ' + str(ReLU[img][0]) + ' **')
         PoolingOfImg.append(ReLU[img][0])
 
         # unit: each image
@@ -209,12 +212,13 @@ def pooling(ReLU, poolFiltSize, stride, option):
             poolingData.append(temp)
                 
             # print result
-            for j in range(len(temp)):
-                printStr = '['
-                for k in range(len(temp[0])):
-                    printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
-                print(str(printStr) + ' ]')
-            print('')
+            if prt != 0:
+                for j in range(len(temp)):
+                    printStr = '['
+                    for k in range(len(temp[0])):
+                        printStr += (' ' + str(round(temp[j][k], 2)) + ' ')
+                    print(str(printStr) + ' ]')
+                print('')
         PoolingOfImg.append(poolingData)
             
         Pooling.append(PoolingOfImg)
@@ -222,7 +226,7 @@ def pooling(ReLU, poolFiltSize, stride, option):
     return Pooling
 
 # make filters for CNN
-def makeFilters(data, rows, cols, images, filtersize, filters):
+def makeFilters(data, rows, cols, images, filtersize, filters, prt):
     # generate filters
     flts = [] # filter array
     i = 0
@@ -259,7 +263,7 @@ def makeFilters(data, rows, cols, images, filtersize, filters):
     return flts
 
 # CNN
-def CNN(data, rows, cols, images, filtersize, filters, flts):
+def CNN(data, rows, cols, images, filtersize, filters, flts, prt):
 
     # data : image data
     # rows : number of rows in each image
@@ -269,24 +273,27 @@ def CNN(data, rows, cols, images, filtersize, filters, flts):
     # testimgdata : image for testing CNN
 
     # convolution 0
-    print('******** CONVOLUTION 0 RESULT ********')
-    convol0 = convolution(data, rows, cols, images, filtersize, filters, flts)
+    if prt != 0: print('******** CONVOLUTION 0 RESULT ********')
+    convol0 = convolution(data, rows, cols, images, filtersize, filters, flts, prt)
 
     # ReLU 0
-    print('')
-    print('******** RELU 0 RESULT ********')
-    ReLU0 = ReLU(convol0)
+    if prt != 0: 
+        print('')
+        print('******** RELU 0 RESULT ********')
+    ReLU0 = ReLU(convol0, prt)
 
     # pooling 0
     # filter size = 2, stride = 2 and max pooling(option=0)
-    print('')
-    print('******** POOLING 0 RESULT ********')
-    pooling0 = pooling(ReLU0, 2, 2, 0)
+    if prt != 0: 
+        print('')
+        print('******** POOLING 0 RESULT ********')
+    pooling0 = pooling(ReLU0, 2, 2, 0, prt)
     
     # convolution 1
     # convolution of pooling 0 result, using only flts[0] filter
-    print('')
-    print('******** CONVOLUTION 1 RESULT ********')
+    if prt != 0: 
+        print('')
+        print('******** CONVOLUTION 1 RESULT ********')
 
     # make 'pooling0' available for 'convolution' function
     pooling0Data = []
@@ -299,18 +306,20 @@ def CNN(data, rows, cols, images, filtersize, filters, flts):
 
     # using only filter 0 when doing 'convolution 1'
     newFilter = [[[1, -1], [-1, 1]]]
-    convol1 = convolution(pooling0Data, math.floor((rows-1)/2), math.floor((cols-1)/2), images*filters, 2, 1, newFilter)
+    convol1 = convolution(pooling0Data, math.floor((rows-1)/2), math.floor((cols-1)/2), images*filters, 2, 1, newFilter, prt)
 
     # ReLU 1
-    print('')
-    print('******** RELU 1 RESULT ********')
-    ReLU1 = ReLU(convol1)
+    if prt != 0:
+        print('')
+        print('******** RELU 1 RESULT ********')
+    ReLU1 = ReLU(convol1, prt)
 
     # pooling 1
     # filter size = 2, stride = 2 and max pooling(option=0)
-    print('')
-    print('******** POOLING 1 RESULT ********')
-    pooling1 = pooling(ReLU1, 2, 2, 0)
+    if prt != 0:
+        print('')
+        print('******** POOLING 1 RESULT ********')
+    pooling1 = pooling(ReLU1, 2, 2, 0, prt)
 
     # result array
     result = []
@@ -323,26 +332,27 @@ def CNN(data, rows, cols, images, filtersize, filters, flts):
         result.append(temp)
 
     # print result array
-    print('******** RESULT ARRAY ********')
-    print('')
-    for i in range(len(result)):
-        print('< image symbol: ' + str(data[i][0]) + ' >')
-        printStr = '['
-        for j in range(len(result[0])):
-            printStr += (' ' + str(round(result[i][j], 2)) + ' ')
-        print(str(printStr) + ' ]')
+    if prt != 0:
+        print('******** RESULT ARRAY ********')
+        print('')
+        for i in range(len(result)):
+            print('< image symbol: ' + str(data[i][0]) + ' >')
+            printStr = '['
+            for j in range(len(result[0])):
+                printStr += (' ' + str(round(result[i][j], 2)) + ' ')
+            print(str(printStr) + ' ]')
 
     return result
                 
-(data, rows, cols, images, filtersize, filters, testimgdata) = getData()
+(data, rows, cols, images, filtersize, filters, testimgdata, prt) = getData()
 
 print('######## making CNN array ########')
 print('')
 
 # make filters
-flts = makeFilters(data, rows, cols, images, filtersize, filters)
+flts = makeFilters(data, rows, cols, images, filtersize, filters, prt)
 # CNN using the filter - data image
-resultArrays = CNN(data, rows, cols, images, filtersize, filters, flts)
+resultArrays = CNN(data, rows, cols, images, filtersize, filters, flts, prt)
 
 # modify resultArrays -> sum = 1
 for i in range(len(resultArrays)):
@@ -352,19 +362,19 @@ for i in range(len(resultArrays)):
     for j in range(len(resultArrays[0])):
         resultArrays[i][j] /= Sum
 
-print('')
+if prt != 0: print('')
 print('######## test image ########')
 print('')
 
 # CNN using the filter - test image
-testArray = CNN([testimgdata], rows, cols, 1, filtersize, filters, flts)
+testArray = CNN([testimgdata], rows, cols, 1, filtersize, filters, flts, prt)
 
 # return final prediction
 maxVal = 0
 maxIndex = -1
 
 # print modified resultArrays
-print('')
+if prt != 0: print('')
 print('**** modified result array ****')
 print('')
 for i in range(len(resultArrays)):
@@ -373,15 +383,52 @@ for i in range(len(resultArrays)):
     for j in range(len(resultArrays[0])):
         printStr += (' ' + str(round(resultArrays[i][j], 2)) + ' ')
     print(str(printStr) + ' ]')
+    print('')
 
-print('')
+# make list of sumProduct
+listOfSumProduct = [] # list of symbol and sumproduct
 for i in range(images):
     sumProduct = 0
     for j in range(len(testArray[0])):
         sumProduct += resultArrays[i][j] * testArray[0][j]
-    print('sumProduct of image ' + data[i][0] + ': ' + str(round(sumProduct, 6)))
-    if sumProduct > maxVal:
-        maxVal = sumProduct
-        maxIndex = i
+    listOfSumProduct.append([data[i][0], sumProduct])
 
-print('final prediction of ' + str(testimgdata[0]) + ': ' + data[maxIndex][0])
+# print list of sumProduct
+for i in range(len(listOfSumProduct)):
+    print('sumProduct of image ' + str(listOfSumProduct[i][0]) + ': ' + str(round(listOfSumProduct[i][1], 6)))
+
+# group for each symbol
+listOfSumProduct.sort(key=lambda x:x[0])
+
+groupedListOfSumProduct = [] # info about each symbol group
+count = 0 # count of each symbol
+Sum = 0 # sum of sumProduct of each symbol
+
+for i in range(len(listOfSumProduct)):
+    count += 1
+    Sum += listOfSumProduct[i][1]
+    symbol = listOfSumProduct[i][0] # symbol of item
+    
+    if i < len(listOfSumProduct)-1:
+        if listOfSumProduct[i+1][0] == symbol: continue
+
+    # execute if new symbol
+    groupedListOfSumProduct.append([symbol, Sum/count])
+    count = 0
+    Sum = 0
+
+# find max average group
+maxGroup = 0
+maxVal = 0
+for i in range(len(groupedListOfSumProduct)):
+    if groupedListOfSumProduct[i][1] > maxVal:
+        maxVal = groupedListOfSumProduct[i][1]
+        maxGroup = i
+
+# print grouped list of sumProduct
+print('')
+for i in range(len(groupedListOfSumProduct)):
+    print('avg sumProduct of symbol ' + str(groupedListOfSumProduct[i][0]) + ': ' + str(round(groupedListOfSumProduct[i][1], 6)))
+
+print('')
+print('final prediction of ' + str(testimgdata[0]) + ': ' + str(groupedListOfSumProduct[maxGroup][0]))
