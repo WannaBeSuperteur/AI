19c5d9b84235910e1939189668fbd2302e17de53
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2019-01-23 01:28:09 +0900

    Add files via upload

--- QLearning.py
+++ QLearning.py
@@ -31,6 +31,73 @@ def lookup(table, coln, value):
         if table[i][coln] == value: return i
     return -1
 
+# Q Learning
+def QLearning_(rewards, moves, state, doRandom, times, lastModifiedTimes):
+    # print
+    symbol = rewards[state][0] # symbol of state
+    print('')
+    print('symbol of state: ' + symbol)
+    for i in range(len(moves)): print(moves[i])
+
+    # find all possible actions
+    nextstate = []
+    for i in range(len(moves)):
+        if moves[i][0] == symbol: nextstate.append(moves[i])
+    print('possible next moves: ' + str(nextstate))
+
+    # decide next action
+    nextActionIndex = -1 # index of next action
+    # RANDOM
+    if random.random() < doRandom:
+        print('decide RANDOM')
+        next_ = random.randint(0, len(nextstate)-1)
+        state = lookup(rewards, 0, nextstate[next_][1]) # update state
+        nextActionIndex = next_
+    # BEST state
+    else:
+        print('decide BEST')
+        bestVal = 0
+        bestIndex = -1
+        for i in range(len(nextstate)):
+            if nextstate[i][2] > bestVal:
+                bestVal = nextstate[i][2]
+                bestIndex = i
+
+        # best of next state (same value as best choice)
+        # choose best next state randomly
+        bestNextState = []
+        for i in range(len(nextstate)):
+            if nextstate[i][2] == bestVal:
+                bestNextState.append(nextstate[i])
+        selectBestNext = random.randint(0, len(bestNextState)-1)
+                
+        state = lookup(rewards, 0, bestNextState[selectBestNext][1]) # update state
+        nextActionIndex = i
+
+    # get immediate reward r(s, a)
+    r = rewards[state][1]
+
+    # find new state s' -> y*max(a')(Q(s', a'))
+    symbol = rewards[state][0]
+    maxReward = 0 # next action that has max reward 
+    for i in range(len(moves)):
+        if moves[i][0] == symbol:
+            if moves[i][2] > maxReward: maxReward = moves[i][2]
+
+    # update state
+    value = r + 0.5 * maxReward
+    if nextstate[nextActionIndex][2] != value:
+        lastModifiedTimes = times
+        nextstate[nextActionIndex][2] = value
+        print('update ' + str(nextstate[nextActionIndex][0]) + ' to '
+            + str(nextstate[nextActionIndex][1]) + ': ' + str(value))
+
+    print('symbol of state : ' + symbol)
+    print('immediate reward: ' + str(r))
+    print('max reward      : ' + str(maxReward))
+
+    return (rewards, moves, state, times, lastModifiedTimes)
+
 # do Q Learning
 def QLearning(rewards, moves):
     # print
@@ -49,69 +116,7 @@ def QLearning(rewards, moves):
         # do Q learning until the last state
         state = 0
         while state != len(rewards)-1:
-
-            # print
-            symbol = rewards[state][0] # symbol of state
-            print('')
-            print('symbol of state: ' + symbol)
-            for i in range(len(moves)): print(moves[i])
-
-            # find all possible actions
-            nextstate = []
-            for i in range(len(moves)):
-                if moves[i][0] == symbol: nextstate.append(moves[i])
-            print('possible next moves: ' + str(nextstate))
-
-            # decide next action
-            nextActionIndex = -1 # index of next action
-            # RANDOM
-            if random.random() < doRandom:
-                print('decide RANDOM')
-                next_ = random.randint(0, len(nextstate)-1)
-                state = lookup(rewards, 0, nextstate[next_][1]) # update state
-                nextActionIndex = next_
-            # BEST state
-            else:
-                print('decide BEST')
-                bestVal = 0
-                bestIndex = -1
-                for i in range(len(nextstate)):
-                    if nextstate[i][2] > bestVal:
-                        bestVal = nextstate[i][2]
-                        bestIndex = i
-
-                # best of next state (same value as best choice)
-                # choose best next state randomly
-                bestNextState = []
-                for i in range(len(nextstate)):
-                    if nextstate[i][2] == bestVal:
-                        bestNextState.append(nextstate[i])
-                selectBestNext = random.randint(0, len(bestNextState)-1)
-                
-                state = lookup(rewards, 0, bestNextState[selectBestNext][1]) # update state
-                nextActionIndex = i
-
-            # get immediate reward r(s, a)
-            r = rewards[state][1]
-
-            # find new state s' -> y*max(a')(Q(s', a'))
-            symbol = rewards[state][0]
-            maxReward = 0 # next action that has max reward 
-            for i in range(len(moves)):
-                if moves[i][0] == symbol:
-                    if moves[i][2] > maxReward: maxReward = moves[i][2]
-
-            # update state
-            value = r + 0.5 * maxReward
-            if nextstate[nextActionIndex][2] != value:
-                lastModifiedTimes = times
-                nextstate[nextActionIndex][2] = value
-                print('update ' + str(nextstate[nextActionIndex][0]) + ' to '
-                  + str(nextstate[nextActionIndex][1]) + ': ' + str(value))
-
-            print('symbol of state : ' + symbol)
-            print('immediate reward: ' + str(r))
-            print('max reward      : ' + str(maxReward))
+            (rewards, moves, state, times, lastModifiedTimes) = QLearning_(rewards, moves, state, doRandom, times, lastModifiedTimes)
 
         # break when converged
         if lastModifiedTimes * 2 + 10 < times: break
